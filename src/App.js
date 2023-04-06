86123dcefadde802e394b706c9d89c9c5d8f6d10
sidney888 <88256186+sidney888@users.noreply.github.com>
2023-04-06 16:04:37 -0400

    Created Homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
-function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+import React, { Component } from 'react';
+import logo from './assets/spotify.png';
+import './App.css';
+import './styles.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} alt="Spotify Logo" className="logo" />
+        <h1 className="welcome-message">Welcome to CIS4301 DataBases Project</h1>
+      </div>
+    );
+  }
 }
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter } from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
