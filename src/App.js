778959bfc0c24a4dec2dcd9463a31ceb55174932
marimonl <lauren.marimon@ufl.edu>
2023-04-11 19:28:14 -0400

    Project Def on homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ class App extends Component {
       <div className="App">
         <img src={logo} alt="Spotify Logo" className="logo" />
         <h1 className="welcome-message">Welcome to CIS4301 DataBases Project</h1>
+        <h2 className= "stampdef"> STAMP: Spotify Trend Analysis for Musical Professionals</h2>
+        <p className = "welcome- paragraph"> Welcome to STAMP, our application allows you to create complex queries based on various musical metrics and data from Spotify’s massive database. To start the application, click the image on the right and begin to create your own queries and find trends!
+
+        </p>
       </div>
+      
     );
   }
 }
