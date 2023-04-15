e99f9302180de9c353754618f9bf34bc6848f839
Sidney Tebbal <sidneytebbal@yahoo.com>
2023-04-15 00:07:48 -0400

    added some parameters and tracking queries and conditions

--- src/Pages/AnalysisPage.js
+++ src/Pages/AnalysisPage.js
@@ -1,47 +1,91 @@
 import React, { useState } from 'react';
-import Dropdown from "../Components/Dropdown";
+import Dropdown from '../Components/Dropdown';
 import Button from '../Components/Button';
+import './AnalysisPage.css';
 
 const AnalysisPage = () => {
   const [selectedOption, setSelectedOption] = useState('Frequency');
   const options = ['Daily', 'Weekly', 'Monthly', 'Yearly'];
+
   const [selectedMetric, setSelectedMetric] = useState('Metric');
-  const metrics = ['Average Dancability', 'Average Energy', 'Average Liveness', 'Average Valence', 'Volume'];
+  const metrics = [
+    'Average Danceability',
+    'Average Energy',
+    'Average Liveness',
+    'Average Valence',
+    'Volume',
+  ];
+
   const [selectedType, setSelectedType] = useState('Query Type');
   const type = ['Tracks', 'Albums'];
-  
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
+
+  const [conditions, setConditions] = useState([{ metric: '', operator: '', value: '' }]);
+
+  const addCondition = () => {
+    setConditions([...conditions, { metric: '', operator: '', value: '' }]);
   };
 
-  const handleMetricClick = (metric) => {
-    setSelectedMetric(metric);
+  const updateCondition = (index, field, value) => {
+    const newConditions = [...conditions];
+    newConditions[index][field] = value;
+    setConditions(newConditions);
   };
-  
+
   return (
-    <div className="analysis">
-      <h1 className="input">Input for Trend Analysis</h1>
-      <p className="welcome-paragraph">
-        From the Dropdown menu, adjust the trend question you would like to be analyzed, then click “Create Analysis” to view your trend.
-      </p>
-      <Dropdown className="Frequency"
-        selectedOption={selectedOption}
-        options={options}
-        onOptionClick={handleOptionClick}
-      />  
-      <Dropdown className="Metric"
-        selectedOption={selectedMetric}
-        options={metrics}
-        onOptionClick={handleMetricClick}
-      /> 
-      <Dropdown className="Type"
-        selectedOption={selectedType}
-        options={type}
-        onOptionClick={(option) => setSelectedType(option)}
-      /> 
-      <Button to="TrendsPage">Create Analysis</Button>
+    <div className="analysis-page">
+      <div className="create-queries">
+        <h2>Create Queries</h2>
+        <div className="query-options">
+          <Dropdown
+            className="Frequency"
+            selectedOption={selectedOption}
+            options={options}
+            onOptionClick={(option) => setSelectedOption(option)}
+          />
+          <Dropdown
+            className="Metric"
+            selectedOption={selectedMetric}
+            options={metrics}
+            onOptionClick={(metric) => setSelectedMetric(metric)}
+          />
+          <Dropdown
+            className="Type"
+            selectedOption={selectedType}
+            options={type}
+            onOptionClick={(queryType) => setSelectedType(queryType)}
+          />
+          <Button to="TrendsPage">Create Analysis</Button>
+        </div>
+      </div>
+
+      <div className="create-conditions">
+        <h2>Create Conditions</h2>
+        {conditions.map((condition, index) => (
+          <div key={index} className="condition">
+            <Dropdown
+              className="Metric"
+              selectedOption={condition.metric}
+              options={metrics}
+              onOptionClick={(metric) => updateCondition(index, 'metric', metric)}
+            />
+            <Dropdown
+              className="Operator"
+              selectedOption={condition.operator}
+              options={['>', '<', '=', '≠']}
+              onOptionClick={(operator) => updateCondition(index, 'operator', operator)}
+            />
+            <input
+              type="text"
+              placeholder="Value"
+              value={condition.value}
+              onChange={(e) => updateCondition(index, 'value', e.target.value)}
+            />
+          </div>
+        ))}
+        <Button onClick={addCondition}>Add Condition</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AnalysisPage;
