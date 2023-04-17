818b8b2b5d64a344176c3767035775d9567b1563
Nic Butakow <nicholas@adastralab.com>
2023-04-17 01:00:37 -0400

    Separate track and album metric lists

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const { connAttrs } = require("./config");
+const {
+	numeric_track_metrics,
+	nonnumeric_metrics,
+	track_metrics,
+	metric_sql
+} = require("../src/assets/metrics");
 const express = require("express");
 const oracledb = require("oracledb");
 
@@ -7,17 +13,17 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const tables = [
-			"NBUTAKOW.ALBUM",
-			"NBUTAKOW.ARTIST",
-			"NBUTAKOW.CATEGORIZED_AS",
-			"NBUTAKOW.GENRE",
-			"NBUTAKOW.RELEASES",
-			"EDWARDARZOUMANOV.TRACK",
-			"EDWARDARZOUMANOV.WRITES",
-			"\"LAUREN.MARIMON\".TRACK",
-			"\"LAUREN.MARIMON\".WRITES",
-			"SIDNEYTEBBAL.TRACK",
-			"SIDNEYTEBBAL.WRITES"
+	"NBUTAKOW.ALBUM",
+	"NBUTAKOW.ARTIST",
+	"NBUTAKOW.CATEGORIZED_AS",
+	"NBUTAKOW.GENRE",
+	"NBUTAKOW.RELEASES",
+	"EDWARDARZOUMANOV.TRACK",
+	"EDWARDARZOUMANOV.WRITES",
+	"\"LAUREN.MARIMON\".TRACK",
+	"\"LAUREN.MARIMON\".WRITES",
+	"SIDNEYTEBBAL.TRACK",
+	"SIDNEYTEBBAL.WRITES"
 ];
 
 async function execute(query) {
@@ -42,7 +48,7 @@ async function execute(query) {
 async function count() {
 	try {
 		let count = 0;
-		for (table of tables) {
+		for (const table of tables) {
 			const result = await execute(
 				`SELECT COUNT(*)
 				FROM ${table}`
@@ -55,6 +61,23 @@ async function count() {
 	}
 }
 
+function eval_order(dependencies, root, in_order = null, done = null) {
+	if (in_order === null) {
+		in_order = [];
+	}
+	if (done === null) {
+		done = new Set();
+	}
+	for (const object of root) {
+		if (!done.has(object)) {
+			done.add(object);
+			eval_order(dependencies, dependencies[root], in_order, done);
+			in_order.push(object);
+		}
+	}
+	return in_order;
+}
+
 app.get("/api/count", (req, res) => {
 	count().then(result => {
 		res.json(result);
@@ -64,8 +87,96 @@ app.get("/api/count", (req, res) => {
 });
 
 app.get("/api/analyze", (req, res) => {
-	const data = JSON.parse(req.query.q);
-	execute(
+	const { queries, analyze } = JSON.parse(req.query.q);
+	const dependencies = {};
+	const clauses = {};
+	for (const id in queries) {
+		dependencies[id] = [];
+		for (const condition of queries[id].conditions) {
+			if (condition.agg_function !== null) {
+				dependencies[id].push(condition.value.toString());
+			}
+		}
+	}
+	const order = eval_order(dependencies, analyze.toString());
+	for (const id of order) {
+		const query = queries[id];
+		const clause = {};
+		const query_tables = new Set();
+		let artist_relation = "";
+		clause.from = `FROM NBUTAKOW.ALBUM\n`; // ALBUM is always needed to get ReleaseDate
+		switch (query.type) {
+			case "Tracks":
+				artist_relation = "Writes";
+				query_tables.add("Track");
+				break;
+			case "Albums":
+				artist_relation = "Releases";
+		}
+		for (const condition of query.conditions) {
+			const metric = condition.metric;
+			if (metric == "Number of Artists") {
+				query_tables.add(artist_relation);
+			} else if (metric == "Artist Name") {
+				query_tables.add("Artist");
+				query_tables.add(artist_relation);
+			} else if (metric == "Artist Genre") {
+				query_tables.add("Categorized As");
+				query_tables.add("Artist");
+				query_tables.add(artist_relation);
+			} else if (track_metrics.includes(condition.metric)) {
+				query_tables.add("Track");
+			}
+		}
+		if (query_tables.has("Writes")) {
+			clause.from +=
+				`INNER JOIN (
+				SELECT *
+				FROM EDWARDARZOUMANOV.TRACK
+				INNER JOIN EDWARDARZOUMANOV.WRITES
+				ON TrackID = WrittenTrack
+				UNION ALL
+				SELECT *
+				FROM "LAUREN.MARIMON".TRACK
+				INNER JOIN "LAUREN.MARIMON".WRITES
+				ON TrackID = WrittenTrack
+				UNION ALL
+				SELECT *
+				FROM SIDNEYTEBBAL.TRACK
+				INNER JOIN SIDNEYTEBBAL.WRITES
+				ON TrackID = WrittenTrack
+				) ON AlbumID = PartOf\n`;
+			if (query_tables.has("Artist")) {
+				clause.from += `INNER JOIN NBUTAKOW.ARTIST ON WriterArtist = ArtistID\n`;
+			}
+		} else {
+			if (query_tables.has("Track")) {
+				clause.from +=
+					`INNER JOIN (
+					SELECT *
+					FROM EDWARDARZOUMANOV.TRACK
+					UNION ALL
+					SELECT *
+					FROM "LAUREN.MARIMON".TRACK
+					UNION ALL
+					SELECT *
+					FROM SIDNEYTEBBAL.TRACK
+					) ON AlbumID = PartOf\n`;
+			}
+			if (query_tables.has("Releases")) {
+				clause.from += `INNER JOIN NBUTAKOW.RELEASES ON AlbumID = ReleasedAlbum\n`;
+				if (query_tables.has("Artist")) {
+					clause.from += `INNER JOIN NBUTAKOW.ARTIST ON ReleasingArtist = ArtistID\n`;
+				}
+			}
+		}
+		if (query_tables.has("Categorized As")) {
+			clause.from += `INNER JOIN NBUTAKOW.CATEGORIZED_AS ON ArtistID = CategorizedArtist\n`;
+		}
+		clauses[id] = clause;
+	}
+	res.json(clauses);
+	/*execute(
 		`SELECT Year, COUNT(*)
 		FROM
 		(
@@ -91,7 +202,7 @@ app.get("/api/analyze", (req, res) => {
 		res.json(result.rows);
 	}).catch(err => {
 		res.json({"error": err});
-	});
+	});*/
 });
 
 app.listen(PORT, () => {
--- src/assets/metrics.js
+++ src/assets/metrics.js
@@ -1,34 +1,78 @@
+const numeric_track_metric_sql = {
+	"Track Duration"   : "TrackDuration",
+	"Explicitness"     : "Explicit",
+	"Acousticness"     : "Acousticness",
+	"Danceability"     : "Danceability",
+	"Energy"           : "Energy",
+	"Instrumentalness" : "Instrumentalness",
+	"Liveness"         : "Liveness",
+	"Loudness"         : "Loudness",
+	"Modality"         : "Major",
+	"Speechiness"      : "Speechiness",
+	"Tempo"            : "Tempo",
+	"Valence"          : "Valence"
+};
+
+const numeric_track_metrics = Object.keys(numeric_track_metric_sql);
+
+let numeric_album_metrics = [];
+
+for (const numeric_metric of numeric_track_metrics) {
+	numeric_album_metrics.push("Average " + numeric_metric);
+	numeric_album_metrics.push("Range of " + numeric_metric);
+}
+
+const nonnumeric_metric_sql = [
+	"Release Date"   : "ReleaseDate",
+	"Track Name"     : "TrackName",
+	"Album Name"     : "AlbumName",
+	"Artist Name"    : "ArtistName",
+	"Key Signature"  : "KeySignature",
+	"Time Signature" : "TimeSignature",
+	"Album Type"     : "AlbumType",
+	"Artist Genre"   : "CategorizedGenre"
+];
+
+const nonnumeric_metrics = Object.keys(nonnumeric_metric_sql);
+
 const query_metrics = [
-	"Average Track Duration",
-	"Average Explicitness",
-	"Average Acousticness",
-	"Average Danceability",
-	"Average Energy",
-	"Average Instrumentalness",
-	"Average Liveness",
-	"Average Loudness",
-	"Average Modality",
-	"Average Speechiness",
-	"Average Tempo",
-	"Average Valence",
-	"Average Number of Artists",
+	"Number of Artists",
 	"Volume"
 ];
 
-const aggregate_metrics = query_metrics.concat([
-	"Release Date",
+const query_metrics_track = numeric_track_metrics.concat(query_metrics);
+
+const query_metrics_album = numeric_album_metrics.concat(query_metrics);
+
+const aggregate_metrics = [
+	"Release Date"
+];
+
+const aggregate_metrics_track = query_metrics_track.concat(aggregate_metrics);
+
+const aggregate_metrics_album = query_metrics_album.concat(aggregate_metrics);
+
+const condition_metrics = nonnumeric_metrics.concat([
+	"Number of Artists"
 ]);
 
-const condition_metrics = aggregate_metrics.concat([
+const condition_metrics_track = numeric_track_metrics.concat(condition_metrics);
+
+const condition_metrics_album = numeric_album_metrics.concat(condition_metrics);
+
+const track_metrics = numeric_track_metrics.concat(numeric_album_metrics).concat([
 	"Track Name",
-	"Album Name",
-	"Artist Name",
 	"Key Signature",
-	"Time Signature",
-	"Album Type",
-	"Artist Genre"
+	"Time Signature"
 ]);
 
-exports.query_metrics = query_metrics;
-exports.aggregate_metrics = aggregate_metrics;
-exports.condition_metrics = condition_metrics;
+exports.numeric_track_metrics = numeric_track_metrics;
+exports.nonnumeric_metrics = nonnumeric_metrics;
+exports.query_metrics_track = query_metrics_track;
+exports.query_metrics_album = query_metrics_album;
+exports.aggregate_metrics_track = aggregate_metrics_track;
+exports.aggregate_metrics_album = aggregate_metrics_album;
+exports.condition_metrics_track = condition_metrics_track;
+exports.condition_metrics_album = condition_metrics_album;
+exports.track_metrics = track_metrics;
+exports.metric_sql = {...numeric_track_metric_sql, ...nonnumeric_metric_sql};
